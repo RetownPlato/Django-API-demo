ad1089180afef2b04dfc5cb8b289290b791d5a6e
RetownPlato <allenplato28@gmail.com>
2020-07-05 19:18:58 +0800

    API demo finish.

--- demo/backend/urls.py
+++ demo/backend/urls.py
@@ -1,20 +1,13 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
-# API endpoints
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('leads/',
-        views.LeadList.as_view(),
-        name='lead-list'),
-    path('leads/<int:pk>/',
-        views.LeadDetail.as_view(),
-        name='lead-detail'),
-    path('users/',
-        views.UserList.as_view(),
-        name='user-list'),
-    path('users/<int:pk>/',
-        views.UserDetail.as_view(),
-        name='user-detail')
-])
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'leads', views.LeadViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- demo/backend/views.py
+++ demo/backend/views.py
@@ -1,44 +1,30 @@
 from .models import Lead
 from .serializers import LeadSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'leads': reverse('lead-list', request=request, format=format)
-    })
-
-
-
-class UserList(generics.ListAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-
-class LeadList(generics.ListCreateAPIView):
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-    queryset = Lead.objects.all()
-    serializer_class = LeadSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-
-class LeadDetail(generics.RetrieveUpdateDestroyAPIView):
+class LeadViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                      IsOwnerOrReadOnly]
+                          IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
